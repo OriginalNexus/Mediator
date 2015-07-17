b66dfc9bf40495f5a43bbac7a188ab57e0369ff4
OriginalNexus <razvan.neacsu5@gmail.com>
2015-07-17 10:29:58 +0300

    Added Download page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-window.onscroll=function(){
-	var myHeader = document.getElementsByClassName("header")[0]
-	if (document.body.scrollTop > 1 && !myHeader.classList.contains("small")) {
-		myHeader.classList.add("small");
-	}
-	else if (document.body.scrollTop <= 1 && myHeader.classList.contains("small")){
-		myHeader.classList.remove("small");
-	}
-};
+
+var VERSION = "1.1.0"
+var VERSION_DISPLAY = "Mediator v1.1.0"
+var FILE_PATH = "./download/Mediator_v1.1.0.apk"
+
+function update() {
+	var btns = document.getElementsByClassName("dl-btn")
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].innerHTML = '<span class="glyphicon glyphicon-download-alt"></span> ' + VERSION_DISPLAY;
+		btns[i].href = FILE_PATH;
+	};
+	btns = document.getElementsByClassName("dl-btn-nav")
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].innerHTML = '<span class="glyphicon glyphicon-download-alt"></span> Download ' + VERSION_DISPLAY;
+		btns[i].href = FILE_PATH;
+	};
+}
